1fc36eae3047b5d0fb410cf47fe16f7e7bb2b583
michael <michaelngige@gmail.com>
2023-09-20 11:26:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Michael Ngige';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${M}!'
