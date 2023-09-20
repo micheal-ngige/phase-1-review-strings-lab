a9f6e06b38679df93dc8898b729993f115c7541c
michael <michaelngige@gmail.com>
2023-09-20 11:57:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 const currentUser = 'Michael Ngige';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome, ${M}!'
+const shortGreeting = 'Welcome, &{M}!'
+// you can also use slice method incase of future changes, its the best!
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
